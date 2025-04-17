561c05078618f7c1084be88c6a2bb491c5d293e7
Tanmoy Majumder <141778682+TanmoyMajumder@users.noreply.github.com>
2025-04-18 01:31:56 +0530

    Add files via upload

---
+++ resume/js/resumeBuilder.js
@@ -0,0 +1,428 @@
+var bio = {
+	"name": "Tanmoy Majumder",
+	"role": "Software Developer",
+	"contacts": {
+		"email": "tanmoymajumder128@gmail.com",
+		"github": "Tanmoy Majumder",
+		"twitter": "@Tanmoy_twt",
+		"location": "Agartala, Tripura, India"
+	},
+	"picture": "images/tan.jpg",
+	"welcomeMessage": "I'm a MERN STACK and Software Developer based in AGARTALA.Iam an ECE undergraduate from NIT AGARTALA.I build websites using MERN STACK.I love solving problems and providing "+
+			  "that save people time and money and help their day to day lives. Thanks for stopping by!.",
+	"skills": ["MERN STACK","DATA ANALYST", "JAVA", "PYTHON", "SOFTWARE DEVELOPMENT", "TEST DRIVEN DEVELOPMENT"]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "ECELL EX-CORE TEAM MEMBER",
+			"title": "WEB DEVELOPER",
+			"location": "ECELL NITA, NIT AGARTALA",
+			"datesWorked": "FEBRUARY 2024 - JANUARY 2025",
+			"description": "As a core tech team member at E-Cell NIT Agartala, I contributed to the development and maintenance of the official website, ensuring a responsive and user-friendly interface. I worked on building event pages, integrating registration systems, and streamlining backend functionalities to support major initiatives like E-Summit. Collaborating closely with design and content teams, I helped enhance the overall digital presence of E-Cell through clean UI/UX and efficient web solutions."
+		},
+		{
+			"employer": "BCLUB NITA",
+			"title": "WEB DEVELOPER",
+			"location": "NIT AGARTALA,AGARTALA",
+			"datesWorked": "MAY 2024 - PRESENT",
+			"description": "As a core tech team member of the Business Club at NIT Agartala, I developed and managed the club’s official website, building dynamic and responsive web pages for events, registrations, and updates. I worked closely with cross-functional teams to ensure smooth coordination and timely delivery of digital assets. Through this role, I also honed my leadership skills by mentoring junior members, taking initiative during high-pressure situations, and leading the tech efforts during major events, contributing to the club’s overall digital growth and team success."
+		},
+		{
+			"employer": "OPEN SOURCE CONTRIBUTOR",
+			"title": "WEB DEVELOPER",
+			
+			"datesWorked": "OCTOBER 2024 - PRESENT",
+			"description": "As an open source contributor, I actively collaborated on community-driven projects, submitting pull requests, fixing bugs, and adding new features across repositories. This experience enhanced my skills in version control, clean code practices, and collaborative development while working with global developers. It also strengthened my problem-solving abilities and exposed me to real-world codebases and open source standards."
+		},
+		{
+			"employer": "GIT AND GITHUB CONTRIBUTOR",
+			"title": "LEARNING PHASE",
+			
+			"datesWorked": "FEB 2025 - PRESENT",
+			"description": "As a Git and GitHub contributor, I gained hands-on experience with version control, branch management, and collaborative workflows. I contributed to projects through pull requests, issue tracking, and code reviews, which improved my understanding of team-based development. This journey sharpened my skills in clean coding, commit discipline, and effective collaboration within a developer community."
+		},
+		{
+			"employer": "OUTLIER AI",
+			"title": "FRONTEND DEVELOPER",
+			"location": "WORK FROM HOME",
+			"datesWorked": "APRIL 2024 - JULY 2024",
+			"description": "During my internship at Outlier as a Frontend Developer, I worked on creating responsive, user-friendly interfaces using HTML, CSS, and JavaScript. I collaborated closely with the design and backend teams to implement features, resolve UI issues, and enhance overall performance. The experience not only strengthened my technical skills but also taught me the value of team effort, effective communication, and collaborative problem-solving in delivering polished, real-world products."
+		},
+		{
+			"employer": "PARTICIPATIONS IN DIFFERENT HACKATHONS",
+			"title": "FRONT END DEVELOPER",
+			"datesWorked": "FEB 2024 - PRESENT",
+			"description": "Participating in various state and global hackathons as a Frontend Developer, I contributed to building intuitive and responsive user interfaces under intense time constraints. Working closely with my team, we turned innovative ideas into functional prototypes, ensuring seamless design and usability. Our strong collaboration, clear communication, and shared problem-solving mindset helped us consistently rank among the top 20–30 teams in several competitions, showcasing the power of teamwork and fast-paced development in high-pressure environments."
+		},
+		{
+			"employer": "WORKING ON DIFFERENT PROJECTS",
+			"title": "WEB DEVELOPER",
+			"datesWorked": "APRIL 2024 - PRESENT",
+			"description": "As a Frontend Developer, I’ve worked on a range of projects focused on building responsive, user-friendly interfaces using modern web technologies. Collaborating with cross-functional teams, I contributed to designing, developing, and optimizing web apps that delivered real-world impact. These experiences, combined with our participation in multiple state and global hackathons—where we consistently ranked among the top 20–30 teams—refined my ability to work under pressure, adapt quickly, and leverage team synergy to bring innovative ideas to life."
+		},
+		
+	]
+};
+
+var education = {
+	"schools": [
+		{ "name": "VIVEKANANDA SHISHU NIKETAN",
+			"datesAttended": "2008 - 2020",
+			"location": "BISHALGARH, TRIPURA, INDIA",
+			"degree": "SSC",
+			
+		},
+		{ "name": "UMAKANTA ACADEMY",
+			"datesAttended": "2020 - 2022",
+			"location": "AGARTALA, TRIPURA, INDIA",
+			"degree": "HSC",
+			"major": "PCM",
+			"minor": "PCB",
+		},
+		{ "name": "NIT AGARTALA",
+			"datesAttended": "2023 - 2027",
+			"location": "AGARTALA, TRIPURA, INDIA",
+			"degree": "BTECH",
+			"CGPA": "8.00",
+			"major": "ECE",
+			
+		},
+				
+	],
+	"onlineCourses": [
+		{ "school": "PW",
+			"title": "Object-Oriented Javascript",
+			"completed": "November 2023",
+			"url": ""
+		},
+		{ "school": "UDEMY",
+			"title": "HTML5 Canvas",
+			"completed": "AUGUST 2023",
+			"url": ""
+		},
+		{ "school": "CHAI AUR CODE",
+			"title": "CSS",
+			"completed": "SEPTEMBER 2023",
+			"url": ""
+		},
+		{ "school": "CHAI AUR CODE",
+			"title": "Javascript Basics And Advanced",
+			"completed": "October 2023",
+			"url": ""
+		},	
+		{ "school": "UDEMY",
+			"title": "REACT JS",
+			"completed": "JANUARY 2024",
+			"url": ""
+		},	
+		{ "school": "CHAI AUR CODE",
+			"title": "MERN STACK",
+			"completed": "JAN 2024-PRESENT",
+			"url": ""
+		},
+		{ "school": "PW",
+			"title": "DSA",
+			"completed": "JANUARY 2024-PRESENT",
+			"url": ""
+		},	
+	]
+};
+
+var projects = {
+	"projects": [
+		
+		{
+			"title": "Portfolio Website",
+			"datesWorked": "DECEMBER 2024",
+			"description": "Created an online portfolio website to showcase my skills.",
+			"images": ["images/SHR.jpg"],
+			"url": "https://tanmoypersonalportfolio.netlify.app/?fbclid=PAY2xjawJuFLFleHRuA2FlbQIxMQABp1ZkFk2lQAv-VCDgWZREDfQy-lpmW8Q0g4HEJo5jJ0_wMe-SPEGdmQ6FjeJ-_aem_SOWsI_Nvl252uxPuMW1-lA"
+		},
+		{
+			"title": "AVIATOR GAME",
+			"datesWorked": "JANUARY 2024",
+			"description": "Created a basic gaming aviator website for understanding my javascript skills.",
+			"images": ["images/avia.jpg"],
+			"url": "https://aviator2.netlify.app/"
+		},
+		{
+			"title": "QUIZLET",
+			"datesWorked": "JANUARY 2024",
+			"description": "Created an online QUIZ Website of the basic web development project",
+			"images": ["images/quiz.jpg"],
+			"url": "https://tanmoymajumder.github.io/QUIZZZZ/"
+		},
+		{
+			"title": "CERTIFICATE GENERATOR",
+			"datesWorked": "MARCH 2024",
+			"description": "Created a certificate generator for any work or personal related stuffs according to the user need.",
+			"images": ["images/cer.jpg"],
+			"url": "https://certificategenerator2.netlify.app/"
+		},
+		{
+			"title": "POKEMON POKEDEX",
+			"datesWorked": "JULY 2024",
+			"description": "Designed and developed a colorful, kid-friendly Pokémon Pokédex website with interactive UI, simplified navigation, and responsive design for an engaging learning experience.",
+			"images": ["images/pok.jpg"],
+			"url": "https://tanmoymajumder.github.io/POKEDEX/"
+		},
+		{
+			"title": "3D NAVBAR",
+			"datesWorked": "SEPTEMBER 2024",
+			"description": "Created a 3D NAV BAR.",
+			"images": ["images/3d.jpg"],
+			"url": "https://3dnavbar2.netlify.app/"
+		},
+	]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+if(bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+
+	for(i in bio.skills) {
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+	}
+}
+
+for(i in formattedContactInfo) {
+	$("#topContacts").append(formattedContactInfo[i]);
+	$("#footerContacts").append(formattedContactInfo[i]);
+}
+
+
+function displayWork() {
+
+	if(work.jobs.length > 0) {
+	
+		$("#workExperience").append(HTMLworkStart);
+
+		for(i in work.jobs) {
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+			var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+			var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
+			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+			var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+
+			$(".work-entry:last").append(formattedEmployerWorkTitle);
+			$(".work-entry:last").append(formattedWorkLocation);
+			$(".work-entry:last").append(formattedDatesWorked);
+			$(".work-entry:last").append(formattedWorkDescription);
+		}
+
+	}
+
+}
+
+displayWork();
+
+
+projects.display = function() {
+	if(projects.projects.length > 0) {
+		for(i in projects.projects) {
+			$("#projects").append(HTMLprojectStart);
+
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+			$(".project-entry:last").append(formattedProjectTitle);
+			$(".project-entry:last").append(formattedProjectDates);
+			$(".project-entry:last").append(formattedProjectDescription);
+
+			for(img in projects.projects[i].images) {
+				var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+				$(".project-entry:last").append(formattedProjectImage);
+			}
+			
+
+		}
+	}
+}
+
+projects.display();
+
+education.display = function() {
+	if(education.schools.length > 0 || education.onlineCourses.length > 0) {
+		for(i in education.schools) {
+			$("#education").append(HTMLschoolStart);
+
+			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);			
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+			var formattedSchoolMinor = HTMLschoolMinor.replace("%data%", education.schools[i].minor);
+
+			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+			$(".education-entry:last").append(formattedSchoolDates);
+			$(".education-entry:last").append(formattedSchoolLocation);
+			$(".education-entry:last").append(formattedSchoolMajor);
+			$(".education-entry:last").append(formattedSchoolMinor);
+		}
+
+		if(education.onlineCourses.length > 0) {
+			$("#education").append(HTMLonlineClasses);
+			for(i in education.onlineCourses) {				
+				$("#education").append(HTMLschoolStart);
+				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+				$(".education-entry:last").append(formattedOnlineDates);
+				$(".education-entry:last").append(formattedOnlineURL);
+			}
+		}
+		
+	}
+}
+
+education.display();
+
+/**
+ * Skills Chart
+ */
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        title: {
+            text: 'Skills'
+        },
+        xAxis: {
+            title: {
+                enabled: true,
+                text: 'Used (year)'
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true
+        },
+        yAxis: {
+            title: {
+                text: 'Duration (months)'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 250,
+            y: 0,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+            borderWidth: 1
+        },
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: 'Used in {point.x} for {point.y} month(s)'
+                }
+			}
+		},
+		series: [{
+				
+				name: 'Javascript',
+				color: 'rgba(204, 0, 0, .5)',
+				data: [[2023, 4], [2023.25, 9], [2023.75, 12], [2024.25, 6],[2025, 3]]
+			},
+			{
+				name: 'HTML/CSS',
+				color: 'rgba(153, 51, 0, .5)',
+				data: [[2023, 4.8], [2023.30, 6], [2023.78, 7.5], [2023.90, 11],[2024, 2]]
+			},
+			{
+				name: 'Web Development',
+				color: 'rgba(0, 153, 102, .5)',
+				data: [[2023, 4], [2023.49, 7], [2023.69, 8], [2023.78, 11],[2024.37, 2],[2024, 6],[2024.63, 12],[2025.45, 2]]
+			},
+			
+			{
+				name: 'Java',
+				color: 'rgba(223, 83, 83, .5)',
+				data: [[2024.19, 10.5], [2024.79, 11], [2024.59, 12], [2025.23, 3]]
+			},
+			{
+				name: 'python',
+				color: 'rgba(119, 152, 191, .5)',
+				data: [[2025.29, 1], [2025.45, 3]]
+			},
+			{
+				name: 'SQL',
+				color: 'rgba(119, 152, 80, .5)',
+				data: [[2023, 12], [2024, 1],[2024.93, 10]]
+			},
+			{
+				name: 'MongoDB',
+				color: 'rgba(170, 162, 30, .5)',
+				data: [[2025, 4], [2025.77, 5]]
+			},
+			{
+				name: 'C++',
+				color: 'rgba(0, 102, 0, .5)',
+				data: [[2023, 3],[2023, 6.5],[2024, 6.5], [2025, 5],[2025.13, 6]]
+			},
+			{
+				name: 'C',
+				color: 'rgba(0, 102, 0, .5)',
+				data: [[2023.63, 8]]
+			},
+			{
+				name: 'Data analytics',
+				color: 'rgba(204, 102, 0, .5)',
+				data: [[2026, 2.5]]
+			},
+			{
+				name: 'MERN',
+				color: 'rgba(210, 200, 0, .5)',
+				data: [[2024, 3], [2025, 5]]
+			}]
+	});
+});
+
+
+$("#mapDiv").append(googleMap);
+
+
